b103ace43a540606e80b7f6f6151218a41fb31c2
Navet21 <pablosantanaftv@hotmail.com>
2025-02-09 18:28:37 +0000

    Primera prueba para Cors 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import CentrosList from "./CentrosList";
+import Centros from "./components/Centros"
 
 function App() {
   return (
     <div>
       <h1>Centros</h1>
-      <CentrosList />
+      <Centros/>
     </div>
   );
 }
