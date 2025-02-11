e5aaea2674472ec2ddd306ee30fd80df3149b903
Navet21 <pablosantanaftv@hotmail.com>
2025-02-11 17:34:13 +0000

    Add: Correccion campos vista Bolo 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ function App() {
             <Route element={<FormularioDurante/>} path="/formularioDurante"></Route>
             <Route element={<FormularioDespues/>} path="/formularioDespues"></Route>
       </Routes>
-    <Centros></Centros>
     </>
   );
 }
