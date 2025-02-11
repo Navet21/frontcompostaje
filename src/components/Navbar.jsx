b1c16c685854fb2b0bed5a6bce791b9cbdc80173
Icesito68 <113939920+Icesito68@users.noreply.github.com>
2025-02-11 11:57:29 +0000

    Prueba url

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ export default function Navbar(){
     return(
         <div className="navbar navbar-dark bg-dark">
             <div className="container">
-                <NavLink className="btn btn-outline-primary" to="/frontcompostaje">Composteras</NavLink>
-                <NavLink className="btn btn-outline-primary" to="/bolos">Bolos</NavLink>
-                <NavLink className="btn btn-outline-primary" to="/registros">Registros</NavLink>
+            <NavLink className="btn btn-outline-primary" to="/#/">Composteras</NavLink>
+            <NavLink className="btn btn-outline-primary" to="/#/bolos">Bolos</NavLink>
+            <NavLink className="btn btn-outline-primary" to="/#/registros">Registros</NavLink>
             </div>
         </div>
     )
