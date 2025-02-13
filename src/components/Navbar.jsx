4be1956be5b7164fa97e1351cd03d98589812f5f
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-02-13 16:51:26 +0000

    Fix: navbar z-index

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="bg-green-700 px-6 py-3 flex items-center justify-between">
+    <nav className="z-40 bg-green-700 px-6 py-3 flex items-center justify-between">
       {/* Logo */}
       <div className="flex items-center">
         <img
