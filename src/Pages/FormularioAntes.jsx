d9a9d052aa6cdff063e54910a79fe994555cb3fc
Icesito68 <113939920+Icesito68@users.noreply.github.com>
2025-02-13 09:52:48 +0000

    Fix: Prueba para iPhone

--- src/Pages/FormularioAntes.jsx
+++ src/Pages/FormularioAntes.jsx
@@ -79,7 +79,7 @@ export default function FormularioAntes() {
 
 
       <Dialog className="bg-gray-900" open={open} handler={handleOpen}>
-  <DialogBody className="grid place-items-center gap-4 rounded-lg p-6 max-h-[90vh] overflow-y-auto">
+  <DialogBody className="grid place-items-center gap-4 rounded-lg p-6 pb-20 max-h-[90vh] overflow-y-auto">
     <Typography color="red" variant="h4">
       ¡Importante!
     </Typography>
