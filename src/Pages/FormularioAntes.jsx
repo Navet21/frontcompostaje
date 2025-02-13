f6cbb072b8d38e679965c696bc1c2f6c0e420780
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-02-13 21:27:54 +0000

    Fix: No solo me cargué los botones

--- src/Pages/FormularioAntes.jsx
+++ src/Pages/FormularioAntes.jsx
@@ -168,7 +168,7 @@ export default function FormularioAntes() {
 
           <div className="flex justify-between">
             <Link to={`/`}>
-            <button className="bg-gray-900 px-4 py-2 rounded-lg border border-transparent hover:border-indigo-400 text-white">
+              <button className="bg-gray-900 px-4 py-2 rounded-lg border border-transparent hover:border-indigo-400 text-white">
                 Volver a Composteras
               </button>
             </Link>
