ec5a9836801fcca6ef35c87836af808ffb22c169
Icesito68 <113939920+Icesito68@users.noreply.github.com>
2025-02-14 10:15:28 +0000

    Fix: Mas estilos para el modo claro

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,8 +4,8 @@ import Footer from "./Footer";
 
 const Layout = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-900 transition">
-      <Navbar />
+      <div className="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-900 transition-all duration-500 ease-in-out">
+        <Navbar />
       <main className="flex-grow">
         <Outlet />
       </main>
