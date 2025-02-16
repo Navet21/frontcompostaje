c40e4854f544327c9083ea36d2b26ef14e0f8207
Icesito68 <113939920+Icesito68@users.noreply.github.com>
2025-02-16 13:30:42 +0000

    Fix: Estilo inicial para seleccionar centro

--- src/Pages/SelectCentro.jsx
+++ src/Pages/SelectCentro.jsx
@@ -7,10 +7,13 @@ export default function SelectCentro() {
   if (error) return <p className="text-center text-red-400">Error: {error}</p>;
 
   return (
-    <div className="text-center text-white">
+    <div className="text-center text-black dark:text-white mt-4">
+        <h2 className="mb-4">Selecciona el centro</h2>
       {centroUser.map((centro) => (
         <Link key={centro.id} to={`/${centro.id}`}>
-        <h2 key={centro.id}>{centro.nombre}</h2>
+            <div className="rounded-2xl flex justify-center items-center bg-white dark:bg-gray-700 text-gray-900 dark:text-white p-6 mb-4 shadow">
+              <h2>{centro.nombre}</h2>
+            </div>        
         </Link>
       ))}
     </div>
