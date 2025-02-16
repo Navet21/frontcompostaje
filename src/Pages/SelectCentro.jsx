5b7c4933d1751921b9d8da36341847efd1408d36
Icesito68 <113939920+Icesito68@users.noreply.github.com>
2025-02-16 13:35:02 +0000

    Fix: Nombre algo mas adecuado

--- src/Pages/SelectCentro.jsx
+++ src/Pages/SelectCentro.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function SelectCentro() {
   const { data: centroUser, loading, error } = useFetch(`https://pablo.informaticamajada.es/api/users/1/centros`);
 
-  if (loading) return <p className="text-center text-gray-200">Cargando nombre del centro...</p>;
+  if (loading) return <p className="text-center text-gray-200">Cargando centros...</p>;
   if (error) return <p className="text-center text-red-400">Error: {error}</p>;
 
   return (
