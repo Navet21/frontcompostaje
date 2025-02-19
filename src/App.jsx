72df6fdebc71ed4025dece16aab5813a8ca7b73a
Icesito68 <113939920+Icesito68@users.noreply.github.com>
2025-02-19 07:48:26 +0000

    Add: Provider inicial

--- src/App.jsx
+++ src/App.jsx
@@ -17,32 +17,34 @@ import RegistroDespues from './Pages/RegistroDespues'
 import RegistroCentros from "./Pages/RegistrosCentros";
 import AnalisisAporte from "./Pages/AnalisisAporte";
 import Login from "./components/Login";
+import GlobalProvider from "./components/GlobalProvider";
 
 function App() {
   return (
     <>
-    <Routes>
-    <Route element={<SelectCentro />} path='/'/>
-    <Route path="/" element={<Layout />}>
-            <Route element={<Composteras />} path='/:id'/>
-            <Route element={<Bolos />} path='/bolos' />
-            <Route element={<Login />} path='/login' />
-            <Route element={<Error />} path='/error' />
-            <Route element={<Registros />} path='/registros/:id' />
-            <Route element={<FormularioAntes/>} path="/formularioAntes"></Route>
-            <Route element={<FormularioDurante/>} path="/formularioDurante"></Route>
-            <Route element={<FormularioDespues/>} path="/formularioDespues"></Route>
-            <Route element={<Analisis/>} path="/bolos/:id"></Route>
-            <Route path="/centros" element={<Centros />} />
-            <Route element={<RegistrosBolo/>} path="/registrosBolo/:id"></Route>
-            <Route element={<RegistroAntes/>} path="/registros/:id/antes"></Route>
-            <Route element={<RegistroDurante/>} path="/registros/:id/durantes"></Route>
-            <Route element={<RegistroDespues/>} path="/registros/:id/despues"></Route>
-            <Route element={<RegistroCentros/>} path="/centro/:id/registros"></Route>
-            <Route element={<AnalisisAporte/>} path="/bolos/analisis/:id"></Route>
-            </Route>
-            
+    <GlobalProvider>
+      <Routes>
+        <Route element={<SelectCentro />} path='/'/>
+        <Route path="/" element={<Layout />}>
+          <Route element={<Composteras />} path='/:id'/>
+          <Route element={<Bolos />} path='/bolos' />
+          <Route element={<Login />} path='/login' />
+          <Route element={<Error />} path='/error' />
+          <Route element={<Registros />} path='/registros/:id' />
+          <Route element={<FormularioAntes/>} path="/formularioAntes"></Route>
+          <Route element={<FormularioDurante/>} path="/formularioDurante"></Route>
+          <Route element={<FormularioDespues/>} path="/formularioDespues"></Route>
+          <Route element={<Analisis/>} path="/bolos/:id"></Route>
+          <Route path="/centros" element={<Centros />} />
+          <Route element={<RegistrosBolo/>} path="/registrosBolo/:id"></Route>
+          <Route element={<RegistroAntes/>} path="/registros/:id/antes"></Route>
+          <Route element={<RegistroDurante/>} path="/registros/:id/durantes"></Route>
+          <Route element={<RegistroDespues/>} path="/registros/:id/despues"></Route>
+          <Route element={<RegistroCentros/>} path="/centro/:id/registros"></Route>
+          <Route element={<AnalisisAporte/>} path="/bolos/analisis/:id"></Route>
+        </Route>
       </Routes>
+      </GlobalProvider>
     </>
   );
 }
