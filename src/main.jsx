d83f552b58c3c1ff6719dac5ce6020eb833858ad
Navet21 <pablosantanaftv@hotmail.com>
2025-02-19 19:38:26 +0000

    Correccion durantes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { HashRouter  } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import CentroProvider from "./Providers/CentroProvider"
+import FormulariosProvider from './Providers/FormularioProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
     <HashRouter >
