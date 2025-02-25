738ea191084a7d12d61651cfaf9d7ef0f3f10f2f
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-02-25 19:37:25 +0000

    Revert "Fix: Tamaño de imagenes"
    This reverts commit 37cf59ec6ff266dc30b4b618ab6d84c307894ed7.

--- src/Pages/RegistroAntes.jsx
+++ src/Pages/RegistroAntes.jsx
@@ -65,11 +65,7 @@ export default function RegistroAntes() {
                         {registro.foto && (
                             <div className="mt-4">
                                 <label className="block text-black dark:text-white">Foto</label>
-                                <img
-                                src={`https://pablo.informaticamajada.es/storage/${registro.foto}`}
-                                alt="Registro"
-                                className="w-full h-64 object-contain rounded-md"
-                                />
+                                <img src={`https://pablo.informaticamajada.es/storage/${registro.foto}`} alt="Registro" className="w-full h-32 object-cover rounded-md" />
                             </div>
                         )}
                         <div>
