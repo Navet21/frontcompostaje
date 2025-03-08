19702cc21a7fdd91a4e8e200d3906ceca67fadc5
Icesito <113939920+Icesito68@users.noreply.github.com>
2025-03-08 12:16:52 +0000

    Update: No ver el centro actual

--- src/components/Centros.jsx
+++ src/components/Centros.jsx
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import MapaCentros from "./MapaCentros";
 import CardCentro from "./CardCentros";
@@ -8,7 +7,6 @@ import { CentroContext } from "../Providers/CentroProvider";
 export default function CentrosList() {
   const { data: centros, loading, error } = useFetch("https://pablo.informaticamajada.es/api/centrosPublicos");
   const { centroId } = useContext(CentroContext); // Usamos el contexto
-  const navigate = useNavigate();
 
   if (loading) return <p className="text-center text-gray-200">Cargando centros...</p>;
   if (error) return <p className="text-center text-red-400">Error: {error}</p>;
@@ -20,12 +18,14 @@ export default function CentrosList() {
   }
   console.log(centros);
 
+  const centrosFiltrados = centros.filter((centro) => Number(centro.id) !== Number(centroId));
+
   return (
     <div className="p-4">
       <h2 className="text-black dark:text-white text-2xl font-bold mb-6 text-center">Lista de Centros</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
-        {centros.map((centro) => (
+        {centrosFiltrados.map((centro) => (
           <CardCentro
             key={centro.id}
             id={centro.id}
@@ -34,7 +34,7 @@ export default function CentrosList() {
           />
         ))}
       </div>
-      <MapaCentros></MapaCentros>
+      <MapaCentros />
     </div>
   );
 }
